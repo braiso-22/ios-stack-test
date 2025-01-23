437b6766cd428339428136c5edd137d28e3a98f8
Brais <braisfv22@gmail.com>
2025-01-23 23:05:41 +0100

    exercise done

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Test
+#  iOS stack test
+
+this is an iOS app for testing how Stacks work
+
+The exercise comes from Aris Guimera's course in youtube
+
+[Aris' Course](https://youtu.be/f6WtmTBFNGM?si=c0EfisB7gZC_uSD0)
+
+
+
+
--- Test/ContentView.swift
+++ Test/ContentView.swift
@@ -10,12 +10,43 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack{
+                HStack{
+                    Rectangle()
+                        .fill(Color.blue)
+                    Rectangle()
+                        .fill(Color.orange)
+                    Rectangle()
+                        .fill(Color.yellow)
+                }
+                Rectangle()
+                    .fill(Color.orange)
+            }
+            VStack{
+                HStack{
+                    Circle()
+                        .fill(Color.green)
+                    Rectangle()
+                        .fill(Color.black)
+                    Circle()
+                        .fill(Color.purple)
+                }
+            }
+            VStack{
+                Rectangle()
+                    .fill(Color.orange)
+                HStack{
+                    Rectangle()
+                        .fill(Color.blue)
+                    Rectangle()
+                        .fill(Color.orange)
+                    Rectangle()
+                        .fill(Color.yellow)
+                }
+            }
         }
         .padding()
+        .background(Color.red)
     }
 }
 
